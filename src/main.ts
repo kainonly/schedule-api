c28776fdd0a4917043f0561d1e87a745dab6f681
kain <zhangtqx@vip.qq.com>
2019-06-28 15:13:49 +0800

    Submit

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
+
+NestFactory.create<NestFastifyApplication>(
+  AppModule,
+  new FastifyAdapter(),
+).then(async (app) => {
+  await app.listen(3000);
+});
