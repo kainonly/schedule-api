14731dc67e1ac3f06c2381a1049009964b5dd2e9
kain <zhangtqx@vip.qq.com>
2019-07-31 18:27:33 +0800

    Submit

--- src/api/api-task.controller.ts
+++ src/api/api-task.controller.ts
@@ -1,56 +1,52 @@
 import { Body, Controller, Post, UsePipes } from '@nestjs/common';
 import { DbService } from '../common/db.service';
 import { CurdFactory } from '../common/curd.factory';
+import { Validate, ValidateTypeOfDelete, ValidateTypeOfEdit, ValidateTypeOfGet, ValidateTypeOfLists } from '../helper';
 
 @Controller('api-task')
 export class ApiTaskController {
-  // private curd: CurdFactory;
-  //
-  // constructor(
-  //   private db: DbService,
-  // ) {
-  //   this.curd = DbService.Curd(db.apiTask);
-  // }
-  //
-  // @Post('get')
-  // @UsePipes(GetValidate())
-  // get(@Body() body: any): Promise<any> {
-  //   return this.curd.get(body);
-  // }
-  //
-  // @Post('lists')
-  // @UsePipes(ListsValidate())
-  // async lists(@Body() body: any): Promise<any> {
-  //   return this.curd.lists(body);
-  // }
-  //
-  // @Post('add')
-  // @UsePipes(Validate({
-  //   job_name: V.string().required(),
-  //   cron: V.string().required(),
-  //   status: V.boolean(),
-  // }))
-  // async add(@Body() body: any): Promise<any> {
-  //   return this.curd.add({
-  //     job_name: body.job_name,
-  //     cron: body.cron,
-  //     status: true,
-  //   });
-  // }
-  //
-  // @Post('edit')
-  // @UsePipes(EditValidate({
-  //   job_name: V.string(),
-  //   cron: V.string(),
-  //   status: V.boolean(),
-  // }))
-  // async edit(@Body() body: any): Promise<any> {
-  //   return this.curd.edit(body);
-  // }
-  //
-  // @Post('delete')
-  // @UsePipes(DeleteValidate())
-  // async delete(@Body() body: any): Promise<any> {
-  //   return this.curd.delete(body);
-  // }
+  private curd: CurdFactory;
+
+  constructor(
+    private db: DbService,
+  ) {
+    this.curd = DbService.Curd(db.apiTask);
+  }
+
+  @Post('get')
+  @UsePipes(ValidateTypeOfGet())
+  get(@Body() body: any): Promise<any> {
+    return this.curd.get(body);
+  }
+
+  @Post('lists')
+  @UsePipes(ValidateTypeOfLists())
+  async lists(@Body() body: any): Promise<any> {
+    return this.curd.lists(body);
+  }
+
+  @Post('add')
+  @UsePipes(Validate({
+    required: ['job_name', 'status'],
+  }))
+  async add(@Body() body: any): Promise<any> {
+    return this.curd.add({
+      job_name: body.job_name,
+      status: true,
+    });
+  }
+
+  @Post('edit')
+  @UsePipes(ValidateTypeOfEdit({
+    required: ['job_name', 'status'],
+  }))
+  async edit(@Body() body: any): Promise<any> {
+    return this.curd.edit(body);
+  }
+
+  @Post('delete')
+  @UsePipes(ValidateTypeOfDelete())
+  async delete(@Body() body: any): Promise<any> {
+    return this.curd.delete(body);
+  }
 }
