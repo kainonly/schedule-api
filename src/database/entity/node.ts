65c6ee17cfd513520cd1436b41bb7435db3e6383
kain <kainpro@foxmail.com>
2019-08-05 12:01:17 +0800

    Submit

--- src/database/entity/node.ts
+++ src/database/entity/node.ts
@@ -21,4 +21,32 @@ export class Node extends Base {
     comment: '外网',
   })
   extranet: string;
+
+  @Column('tinyint', {
+    default: 22,
+    unsigned: true,
+    comment: 'SSH端口',
+  })
+  ssh_port: number;
+
+  @Column('text', {
+    comment: 'SSH密钥',
+  })
+  ssh_key: string;
+
+  @Column('tinyint', {
+    default: 0,
+    width: 1,
+    unsigned: true,
+    comment: '是否应用容器',
+  })
+  use_docker?: number;
+
+  @Column('tinyint', {
+    default: 0,
+    width: 1,
+    unsigned: true,
+    comment: '是否应用网络回调钩子',
+  })
+  use_webhook?: number;
 }
