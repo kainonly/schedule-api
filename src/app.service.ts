aaff0a1dea7acae989137870055116f6a200e604
kain <kainpro@foxmail.com>
2019-09-18 22:23:41 +0800

    Submit

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { CronJob } from 'cron';
 
 @Injectable()
 export class AppService {
+  private runtime: Map<string, any> = new Map<string, any>();
+  private jobs: Map<string, CronJob> = new Map<string, CronJob>();
+
+  get(identity: string): any {
+    if (!this.runtime.has(identity)) {
+      return false;
+    }
+    const job = this.jobs.get(identity);
+    return Object.assign(this.runtime.get(identity), {
+      running: job.running,
+      nextDate: job.nextDate(),
+      lastDate: job.lastDate(),
+    });
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import * as fastifyCompress from 'fastify-compress';
 import { AppModule } from './app.module';
 
 NestFactory.create<NestFastifyApplication>(
   AppModule,
   new FastifyAdapter(),
 ).then(async (app) => {
+  app.register(fastifyCompress);
   await app.listen(3000);
 });
