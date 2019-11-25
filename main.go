db142ba75b1dc2dbb21b2b8c9a961d53a70e1f6b
kain <zhangtqx@vip.qq.com>
2019-11-25 11:08:46 +0800

    Submit Testing

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/kataras/iris/v12"
+	"github.com/kataras/iris/v12/middleware/logger"
+	"github.com/kataras/iris/v12/middleware/recover"
+	"net/http"
+	_ "net/http/pprof"
+	"schedule-api/router"
+	"schedule-api/schedule"
+)
+
+func main() {
+	go func() {
+		http.ListenAndServe(":6060", nil)
+	}()
+	app := iris.New()
+	app.Logger().SetLevel("debug")
+	app.Use(recover.New())
+	app.Use(logger.New())
+	routes := router.Init(
+		schedule.InjectSchedule(),
+	)
+	app.Post("put", routes.PutRoute)
+	app.Run(iris.Addr(":3000"), iris.WithoutServerError(iris.ErrServerClosed))
+}
