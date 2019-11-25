465d6fa491bbb045c61cc8232799407f92ac9972
kain <kainpro@foxmail.com>
2019-11-25 12:00:24 +0800

    Submit Put

--- common/common.go
+++ common/common.go
@@ -1,6 +1,13 @@
 package common
 
 type TaskOption struct {
+	Identity string                 `json:"identity" validate:"required"`
+	TimeZone string                 `json:"time_zone" validate:"required"`
+	Start    bool                   `json:"start" validate:"required"`
+	Entries  map[string]EntryOption `json:"entries" validate:"required,dive,required"`
+}
+
+type EntryOption struct {
 	CronTime string            `json:"cron_time" validate:"required"`
 	Url      string            `json:"url" validate:"required,url"`
 	Headers  map[string]string `json:"headers"`
