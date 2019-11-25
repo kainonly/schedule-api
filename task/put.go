24f9971d71cd7bb40b93a106af7db0335519e3b1
kain <zhangtqx@vip.qq.com>
2019-11-25 17:09:37 +0800

    Submit Put

--- task/put.go
+++ task/put.go
@@ -1,30 +1,50 @@
 package task
 
 import (
+	"github.com/parnurzeal/gorequest"
 	"github.com/robfig/cron/v3"
 	"schedule-api/common"
 	"time"
 )
 
-func (c *Task) Put(identity string, option common.TaskOption) (err error) {
+func (c *Task) Put(option common.TaskOption) (err error) {
+	identity := option.Identity
 	timezone, err := time.LoadLocation(option.TimeZone)
 	if err != nil {
 		return
 	}
 	c.runtime[identity] = cron.New(cron.WithSeconds(), cron.WithLocation(timezone))
-	for key, entry := range option.Entries {
-		var entryId cron.EntryID
-		entryId, err = c.runtime[identity].AddFunc(entry.CronTime, func() {
-			println("abc")
-		})
-		if err != nil {
-			return
-		}
-		println(key)
-		println(entryId)
+	c.options[identity] = option.Entries
+	c.entries[identity] = make(map[string]cron.EntryID)
+	for key := range option.Entries {
+		go c.webhook(identity, key)
 	}
 	if option.Start {
 		c.runtime[identity].Start()
 	}
 	return
+}
+
+func (c *Task) webhook(identity string, key string) {
+	var err error
+	option := c.options[identity][key]
+	c.entries[identity][key], err = c.runtime[identity].AddFunc(option.CronTime, func() {
+		agent := gorequest.New().Post(option.Url)
+		if option.Headers != nil {
+			for key, value := range option.Headers {
+				agent.Set(key, value)
+			}
+		}
+		if option.Body != nil {
+			agent.Send(option.Body)
+		}
+		_, body, errs := agent.End()
+		if errs != nil {
+			return
+		}
+		println(body)
+	})
+	if err != nil {
+		return
+	}
 }
--- task/task.go
+++ task/task.go
@@ -1,13 +1,20 @@
 package task
 
-import "github.com/robfig/cron/v3"
+import (
+	"github.com/robfig/cron/v3"
+	"schedule-api/common"
+)
 
 type Task struct {
 	runtime map[string]*cron.Cron
+	options map[string]map[string]common.EntryOption
+	entries map[string]map[string]cron.EntryID
 }
 
 func InjectSchedule() *Task {
 	task := new(Task)
 	task.runtime = make(map[string]*cron.Cron)
+	task.options = make(map[string]map[string]common.EntryOption)
+	task.entries = make(map[string]map[string]cron.EntryID)
 	return task
 }
