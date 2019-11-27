f569cf173ff57f3aa4cb695c5ae3811fb910c386
kain <kainpro@foxmail.com>
2019-11-27 17:03:33 +0800

    Submit Travis

---
+++ scripts/ci-tag.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Login docker
+echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
+# Build Golang Application
+go build -o dist/schedule-api
+# Build docker image
+docker build . -t kainonly/schedule-api:${TRAVIS_TAG}
+# Push docker image
+docker push kainonly/schedule-api:${TRAVIS_TAG}
